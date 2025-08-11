e07fc73a89f74cf0c842442969a0dcaf28d2d0b8
Scheeza-Ahmad <scheezaahmad117@gmail.com>
2025-08-12 03:46:57 +0500

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import  "./Footer.css";
+const Footer = () => {
+  return (
+    <div className='footer'>
+      <h5>CopyRight reserves @Scheeza Ahmad 2018</h5>
+    </div>
+  )
+}
+
+export default Footer
---
+++ Navbar.jsx
@@ -0,0 +1,20 @@
+//agar rafce likho ga to boiler plate code aa jai ga agar react ki extentions download ki hui hain
+
+import React from 'react'
+import  "./Navbar.css"
+const Navbar = () => {
+  return (
+    <div>
+        <nav>
+            <ul className='nav'>
+                <li>Home</li>
+                <li>Contact</li>
+                <li>About</li>
+            </ul>
+        </nav>
+      
+    </div>
+  )
+}
+
+export default Navbar;
