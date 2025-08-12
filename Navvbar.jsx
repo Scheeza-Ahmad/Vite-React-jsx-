8f157747ffd8712db8bb45ab589d7d84cd27247d
Scheeza-Ahmad <scheezaahmad117@gmail.com>
2025-08-13 03:05:39 +0500

    Create Navvbar.jsx

---
+++ Navvbar.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Link } from "react-router-dom";
+const Navbar = () => {
+  return (
+    <div>
+      <nav>
+        <Link to="/">
+          <li>Home</li>
+        </Link>
+        <Link to="/contact">
+          <li>Contact</li>
+        </Link>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
