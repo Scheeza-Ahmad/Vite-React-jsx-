337930b394cf718309e708a885c3877a527ccd43
Scheeza-Ahmad <scheezaahmad117@gmail.com>
2025-08-13 20:08:55 +0500

    Update Home.jsx

--- Home.jsx
+++ Home.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import { Link, Outlet } from "react-router-dom";
 
 const Home = () => {
   return (
-    <div>
-      Home
-    </div>
-  )
-}
+    <>
+      <nav className="bg-blue-500 p-4 text-white">
+        <ul className="flex justify-around gap-4">
+          <li><Link to="/" className="hover:underline">Home</Link></li>
+          <li><Link to="ContactUs" className="hover:underline">Contact Us</Link></li>
+          <li><Link to="AboutUs" className="hover:underline">About</Link></li>
+        </ul>
+      </nav>
+      <div className="p-6">
+        <Outlet />
+      </div>
+    </>
+  );
+};
 
-export default Home
+export default Home;
